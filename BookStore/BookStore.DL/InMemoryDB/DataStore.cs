f7e21b10b7727fdfbe06e7d61a8bec7c7ef0a10b
Nik0la10 <nikola_p.10@abv.bg>
2023-02-23 19:19:58 +0200

    23.02.2023

---
+++ BookStore/BookStore.DL/InMemoryDB/DataStore.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DL.InMemoryDB
+{
+    public class DataStore
+    {
+        List<Author>
+    }
+}
